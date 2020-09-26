6272578ac8cee227ee5d7357f9988fbf39dbfc5e
Dhairya_prj <dhairyahp15@gmail.com>
2020-09-25 19:39:06 -0500

    Completed Phase 1a. Basic Objects with proper comments, methods, and also edited README.txt

--- Plant.java
+++ Plant.java
@@ -1,33 +1,68 @@
-
-/**
- * Write a description of class Plant here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Plant
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Plant
-     */
+    // Instance variables decalred below.
+    private String name;
+    private int height; // it gives us current height.
+    
+    // is the plant edible or not.
+    private boolean edible;
+    
+    //Default constructor that assigns name, height and edible to default values.
     public Plant()
     {
-        // initialise instance variables
-        x = 0;
+        name = "No Name Plant";
+        height = 0;
+        edible = false;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    // Parameterized which only accepts and assigns the name.
+    public Plant(String name)
     {
-        // put your code here
-        return x + y;
+        this.name = name; // this.name is used to differentiate between local and instance variable.
+        height = 0;
+        edible = false;
     }
+    
+    // Parameterized which accepts and assigns all the instance variables.
+    public Plant(String name, int height, boolean edible)
+    {
+        // again here this. is used to differentiate between local and instance variables.
+        this.name = name;
+        this.height = height;
+        this.edible = edible;
+    }
+    
+    // -+-+-+ Accessor Methods for each variable -+-+-+
+    
+    public String getName()
+    {
+        return name;
+    } // returns name.
+    
+    public int getHeight()
+    {
+        return height;
+    } // returns height in centimeter.
+    
+    public boolean getEdible()
+    {
+        return edible;
+    } // returns true or false as in edible or not.
+    
+    // -+-+-+ Accessor Methods for each variable -+-+-+
+    
+    public void setName(String newName)
+    {
+        name = newName;
+    } // sets the current name to newName.
+    
+    public void setHeight(int height)
+    {
+        this.height = height;
+    } // sets the current height to the height passsed as a parameter.
+    
+    public void setEdible(boolean newEdible)
+    {
+        edible = newEdible;
+    } // sets current boolean value of edible to newEdible.
 }
